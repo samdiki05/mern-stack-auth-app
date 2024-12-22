ada5cb43cb475be7b5ede2f19445963617a0b40c
ZeeshuCoder <codewithzeeshu122@gmail.com>
2024-12-22 12:36:52 +0500

    HOME and OTP Page Updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import Hero from "../components/Hero";
 import Instructor from "../components/Instructor";
 import Technologies from "../components/Technologies";
@@ -10,42 +10,7 @@ import { Navigate, useNavigate } from "react-router-dom";
 import Footer from "../layout/Footer";
 
 const Home = () => {
-  const { isAuthenticated, setIsAuthenticated, setUser } =
-    useContext(Context);
-
-  console.log(isAuthenticated);
-  const navigateTo = useNavigate();
-
-  const logout = async () => {
-    await axios
-      .get("http://localhost:4000/api/v1/user/logout", {
-        withCredentials: true,
-      })
-      .then((res) => {
-        toast.success(res.data.message);
-        setUser(null);
-        setIsAuthenticated(false);
-      })
-      .catch((err) => {
-        toast.error(err.response.data.message);
-        console.error(err);
-      });
-  };
-
-  if (!isAuthenticated) {
-    return <Navigate to={"/auth"} />;
-  }
-  return (
-    <>
-      <section className="home">
-        <Hero />
-        <Instructor />
-        <Technologies />
-        <Footer/>
-        <button onClick={logout}>Logout</button>
-      </section>
-    </>
-  );
+  return <></>;
 };
 
 export default Home;
